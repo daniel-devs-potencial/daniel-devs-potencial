ed33a83cea07ca564a83541dd3e5da3b73922ecf
primeiro projeto <prraymundo_@outlook.com>
2026-03-15 13:11:58 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,129 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+
+    const currencyValueConverted = document.querySelector(".currency-value")
+
+
+     
+    const dolarToday = 5.2
+    const euroToday = 6.2
+    const libraToday = 7.2
+
+
+
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+
+
+        }).format(inputCurrencyValue / dolarToday)
+
+    }
+
+    if (currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("gb-ENG", {
+            style: "currency",
+            currency: "GBP"
+
+
+        }).format(inputCurrencyValue / dolarToday)
+
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+
+        }).format(inputCurrencyValue / euroToday)
+
+
+    }
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+
+    }).format(inputCurrencyValue)
+
+}
+
+
+
+
+
+
+
+
+function changeCurrency() {
+
+    const currencyName = document.getElementById("currency-name")
+    const currencyImg = document.querySelector(".currency-img")
+
+
+
+
+
+
+    if (currencySelect.value == "dolar") {
+
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = "assets/usa.png"
+
+    }
+
+    if (currencySelect.value == "euro") {
+
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "assets/euro.png"
+
+    }
+
+    if (currencySelect.value == "real") {
+
+        currencyName.innerHTML = "Real"
+        currencyImg.src = "assets/real.png"
+
+    }
+
+    if (currencySelect.value == "libra") {
+
+        currencyName.innerHTML = "Libra"
+        currencyImg.src = "assets/libra.png"
+
+    }
+
+
+
+
+
+
+
+    convertValues()
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
+
+
